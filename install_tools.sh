9a7473478ddf1ea3eb4c9de24f7020c628ef445a
Leon Hennings <leon.hennings@tillvaxtverket.se>
2015-10-29 10:41:13 +0100

    Add file remembering/downloading some tools i like

---
+++ install_tools.sh
@@ -0,0 +1,5 @@
+#ohmyzsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install fpp prettyping thefuck archey
