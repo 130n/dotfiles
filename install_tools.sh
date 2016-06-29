0e4209a7f80ad02e29bd2de7186d8b6731d72a51
Leon Hennings <leon.hennings@tillvaxtverket.se>
2016-06-29 16:29:45 +0200

    Update install tools with node, npm and more brew stuff

--- install_tools.sh
+++ install_tools.sh
@@ -1,5 +1,12 @@
 #ohmyzsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 #homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install fpp prettyping thefuck archey
+
+brew install thefuck archey fpp node tpp wget git htop-osx sl zsh prettyping
+
+gem install bropages
+
+brew install node
+npm install npm -g
